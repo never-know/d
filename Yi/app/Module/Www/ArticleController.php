a097b00c8b7ed08a0076aaf6721b015acc95569a
never-know <932338320@qq.com>
2017-02-08 22:18:35 +0800

    fix

--- Yi/app/Module/Www/ArticleController.php
+++ Yi/app/Module/Www/ArticleController.php
@@ -21,12 +21,12 @@ class ArticleController extends \Min\Controller
 	
 	public function edit_post()
 	{
-		$param['title'] = addslashes($_POST['title']);
-		$param['desc'] 	= addslashes($_POST['desc']);
-		$param['icon'] 	= addslashes($_POST['icon']);
+		$param['title'] = trim($_POST['title']);
+		$param['desc'] 	= trim($_POST['desc']);
+		$param['icon'] 	= $_POST['icon'];
 		
-		if (!\validate('length', $param['title'], 32)) 	$this->error(1, '标题最多包含30个字符');
-		if (!\validate('length', $param['desc'], 64)) 	$this->error(1, '简介最多包含60个字符');
+		if (!\validate('length', $param['title'], 32)) 	$this->error(1, '标题最多包含32个字符');
+		if (!\validate('length', $param['desc'], 64)) 	$this->error(1, '简介最多包含64个字符');
 		if (!\validate('img_url', $param['icon'], 128)) $this->error(1, '图像url不合法');
 		 
 		if (!empty($_POST['date_start']) && !\validate('date_Y-m-d', $_POST['date_start'])) {
@@ -36,8 +36,10 @@ class ArticleController extends \Min\Controller
 			$this->error(1, '结束日期格式错误');
 		}
 		
-		$param['start'] = $_POST['date_start'];
-		$param['end'] 	= $_POST['date_end'];
+		$param['start'] 	= strtr($_POST['date_start'], '-', '');
+		$param['end'] 		= strtr($_POST['date_end'], '-', '');
+		$param['region'] 	= intval($_POST['date_end']);
+		$param['content'] 	= \Min\Xss::filter($_POST['content']);
 
 		$this->response($this->request('\\App\\Service\\Article::add', $param));
 	}
--- Yi/app/Service/ArticleService.php
+++ Yi/app/Service/ArticleService.php
@@ -8,6 +8,10 @@ class ArticleService extends \Min\Service
 
 	public function add($param)
 	{
+		if (!empty($param['id'])) {
+			return $this->eidt($param);
+		}
+		
 		$sql = 'INSERT INTO {article} (tag, start, end, region, title, desc, icon) values ('.
 			implode(',', [intval($param['tag']), intval($param['start']), intval($param['end']), intval($param['region']), ':title', ':desc', ':icon)']);
 
@@ -23,6 +27,20 @@ class ArticleService extends \Min\Service
 			$this->DBManager()->transaction_rollback();
 			return false;
 		}
+	}	
+	
+	public function edit($param)
+	{
+		$param['id'] = intval($param['id']);
+		
+		$sql = 'UPDATE {article}  SET tag = '. intval($param['tag']) . ', start = '. intval($param['start']) . ', end = '. intval($param['end']) . ', region = '. intval($param['region']) . ', title = :title, desc = :desc, icon = :icon  WHERE id = '. $param['id'];
+
+		$result = $this->query($sql, [':title' => $param['title'], ':desc' => $param['desc'],':icon' => $param['icon']]);
+		
+		$sql2 = 'UPDATE {article_content} set content = :content WHERE id = '. $param['id'];
+		$result2 = $this->query($sql2, [':content' => $param['content']]);
+			 
+		return $result && $result2;
 		
 	}	
 	
