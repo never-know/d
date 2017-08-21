91e0d3c6ded2d1c40a46f362126880f827809c78
never-know <932338320@qq.com>
2017-08-21 16:38:48 +0800

    fix

--- Yi/app/Service/ArticleService.php
+++ Yi/app/Service/ArticleService.php
@@ -166,7 +166,7 @@ class ArticleService extends \Min\Service
 				}
 			}
 			
-			if (!empty($p['sub_region']) && preg_match('^\d+(,\d)*$', $p['sub_region'])) {
+			if (!empty($p['sub_region']) && preg_match('/^\d+(,\d)*$/', $p['sub_region'])) {
 				$param['filter']['region'] = ' OR region_id = ' . implode(' OR region_id = ' . explode(',', $p['sub_region']));
 			}
 			
